aa2c224eead418fa8812bf47202b15c2b2482021
John Winters <jhnwin@amazon.com>
2019-05-08 09:51:25 -0700

    scaffold

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>City Explorer</h1>
+      <p>Enter a location below to learn about the weather, events, restaurants, movies filmed there, and more!</p>
+    </header>
+  );
+};
+
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Main />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
